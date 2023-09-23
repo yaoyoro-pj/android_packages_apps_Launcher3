ad3be95af17fc3d28b5a8019ec8a23f60ba173f3
Andy Wickham <awickham@google.com>
2023-09-22 17:44:23 -0700

    Add CUSTOM_LPNH_THRESHOLDS feature flag to customize LPNH
    When this flag is enabled, new Developer Options appear which
    allow you to customize the slop and timeout to invoke LPNH.
    
    The slop is defined as a multiplier to the default edge slop
    (which I discovered while looking into this - it appears to
    be intended for touches along the device edge and is 50%
    larger than the default touch slop; currently used by Back).
    
    Timeout is defined in milliseconds, defaulting to 400, at least
    on my device.
    
    Bug: 301680992
    Bug: 300955321
    Flag: CUSTOM_LPNH_THRESHOLDS - should be no-op with default off
    Test: Manual with flag on and adjusting sliders, and flag off
    Change-Id: Iabc7b3706f4fdd6f0392c858b81a856e375ffd51

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -20,6 +20,7 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.util.Log
+import android.view.ViewConfiguration
 import androidx.annotation.VisibleForTesting
 import com.android.launcher3.LauncherFiles.DEVICE_PREFERENCES_KEY
 import com.android.launcher3.LauncherFiles.SHARED_PREFERENCES_KEY
@@ -278,6 +279,20 @@ class LauncherPrefs(private val encryptedContext: Context) {
         @JvmField
         val ALL_APPS_OVERVIEW_THRESHOLD =
             nonRestorableItem(LauncherAppState.KEY_ALL_APPS_OVERVIEW_THRESHOLD, 180, true)
+        @JvmField 
+        val LONG_PRESS_NAV_HANDLE_SLOP_PERCENTAGE =
+            nonRestorableItem(
+                LauncherAppState.KEY_LONG_PRESS_NAV_HANDLE_SLOP_PERCENTAGE,
+                100,
+                true
+            )
+        @JvmField
+        val LONG_PRESS_NAV_HANDLE_TIMEOUT_MS =
+            nonRestorableItem(
+                LauncherAppState.KEY_LONG_PRESS_NAV_HANDLE_TIMEOUT_MS,
+                ViewConfiguration.getLongPressTimeout(),
+                true
+            )
         @JvmField val THEMED_ICONS = backedUpItem(Themes.KEY_THEMED_ICONS, false, true)
         @JvmField val PROMISE_ICON_IDS = backedUpItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
         @JvmField val WORK_EDU_STEP = backedUpItem(WorkProfileManager.KEY_WORK_EDU_STEP, 0)
