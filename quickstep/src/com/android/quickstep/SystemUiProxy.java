47190350fbb60e0e38d0577d7a54d9697e470149
Andy Wickham <awickham@google.com>
2024-01-08 19:20:37 -0800

    Allow stashed taskbar to be long pressed.
    Bug: 319175012
    Test: Manual and unit test
    Flag: NA
    (see other change)
    
    Change-Id: Ic8ac68d21c64a909a1035339d36f9c3b50a8963c

--- quickstep/src/com/android/quickstep/SystemUiProxy.java
+++ quickstep/src/com/android/quickstep/SystemUiProxy.java
@@ -98,7 +98,7 @@ import java.util.LinkedHashMap;
 /**
  * Holds the reference to SystemUI.
  */
-public class SystemUiProxy implements ISystemUiProxy {
+public class SystemUiProxy implements ISystemUiProxy, NavHandle {
     private static final String TAG = SystemUiProxy.class.getSimpleName();
 
     public static final MainThreadInitializedObject<SystemUiProxy> INSTANCE =
