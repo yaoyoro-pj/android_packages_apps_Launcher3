7c5706759708ceb6ab1c7654d811f44ebe771015
Ido Ben-Hur <idoybh2@gmail.com>
2024-08-31 04:31:36 +0300

    Launcher3: Add a toggle for long press on navbar to search [2/3]
    Also improve isGSAEnabled for readability and efficiency

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -842,10 +842,19 @@ public final class Utilities {
     }
 
     public static boolean isGSAEnabled(Context context) {
+        PackageManager pm = context.getPackageManager();
+        if (pm == null) {
+            return false;
+        }
         try {
-            return context.getPackageManager().getApplicationInfo(GSA_PACKAGE, 0).enabled;
+            return pm.getApplicationInfo(GSA_PACKAGE, 0).enabled;
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
+    }
+
+    public static boolean isLongPressToSearchEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.NAVBAR_LONG_PRESS_GESTURE, 1) == 1;
     }
 }
