586c01acf20fe141277d4ad3592d7b81742775af
ExactExampl <xmpl@dontsleep.work>
2025-07-18 19:21:32 +0300

    Launcher3: implement Circle to search navbar gesture
    * Inspired by https://github.com/yaap/packages_apps_Launcher3/commit/390e32351fc44875096b1d9cffc425350e626d5a, adapted to Voice assistant Omni API, as reverse engineered from QPR1 NexusLauncher

--- quickstep/src/com/android/quickstep/inputconsumers/NavHandleLongPressHandler.java
+++ quickstep/src/com/android/quickstep/inputconsumers/NavHandleLongPressHandler.java
@@ -13,26 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.quickstep.inputconsumers;
 
+import static android.os.VibrationEffect.createPredefined;
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.android.launcher3.R;
 import com.android.launcher3.util.ResourceBasedOverride;
+import com.android.launcher3.util.VibrationUtils;
+import com.android.launcher3.util.VibratorWrapper;
+import com.android.launcher3.Utilities;
 import com.android.quickstep.NavHandle;
+
+import android.os.Bundle;
+import android.os.IBinder;
+
+import com.android.systemui.shared.system.ActivityManagerWrapper;
+
+import static com.android.launcher3.util.Executors.UI_HELPER_EXECUTOR;
 
 /**
  * Class for extending nav handle long press behavior
  */
 public class NavHandleLongPressHandler implements ResourceBasedOverride {
 
+    private final String TAG = "NavHandleLongPressHandler";
+    private final boolean DEBUG = false;
+
+    private final Context mContext;
+
     /** Creates NavHandleLongPressHandler as specified by overrides */
-    public static NavHandleLongPressHandler newInstance(Context context) {
-        return Overrides.getObject(NavHandleLongPressHandler.class, context,
-                R.string.nav_handle_long_press_handler_class);
+    public NavHandleLongPressHandler(Context context) {
+        mContext = context;
     }
 
     /**
@@ -47,6 +62,13 @@ public class NavHandleLongPressHandler implements ResourceBasedOverride {
      * @param navHandle to handle this long press
      */
     public @Nullable Runnable getLongPressRunnable(NavHandle navHandle) {
+        if (Utilities.isGSAEnabled(mContext)) {
+            VibrationUtils.triggerVibration(mContext, 2);
+            if (DEBUG) Log.d(TAG, "getLongPressRunnable: CTS should start now");
+            return () -> UI_HELPER_EXECUTOR.execute(() -> {
+                startVoiceSession(mContext, 1);
+            });
+        }
         return null;
     }
 
@@ -56,6 +78,23 @@ public class NavHandleLongPressHandler implements ResourceBasedOverride {
      * @param navHandle to handle the animation for this touch
      */
     public void onTouchStarted(NavHandle navHandle) {}
+
+    /**
+     * Starts the voice assistant session for Omni with given parameters.
+     *
+     * @param context Android context (for attribution tag)
+     * @param entry_point Entry point code for the session
+     * @return showVoiceSession boolean indicating success/failure
+     */
+    private boolean startVoiceSession(Context context, int entry_point) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("omni.entry_point", entry_point);
+        boolean showVoiceSession = ActivityManagerWrapper.getInstance().showVoiceSession((IBinder) null, bundle, 7, context.getAttributionTag());
+        if (!showVoiceSession) {
+            if (DEBUG) Log.d(TAG, "startVoiceSession: Omni invocation failed: invocation error");
+        }
+        return showVoiceSession;
+    }
 
     /**
      * Called when nav handle gesture is finished by the user lifting their finger or the system
