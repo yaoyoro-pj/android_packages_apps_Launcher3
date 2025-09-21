d9b77a5bab58115ecac021518dad328ee7443620
Michal Brzozowski <miki@extremic.com>
2025-09-21 11:25:48 +0200

    Focus on the most recent task when opening recents
    Change-Id: I58d9be2708306f795ddbd6302405199762b045f9

--- quickstep/src/com/android/quickstep/views/LauncherRecentsView.java
+++ quickstep/src/com/android/quickstep/views/LauncherRecentsView.java
@@ -32,6 +32,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -163,6 +164,10 @@ public class LauncherRecentsView extends RecentsView<QuickstepLauncher, Launcher
             runActionOnRemoteHandles(remoteTargetHandle ->
                     remoteTargetHandle.getTaskViewSimulator().setDrawsBelowRecents(true));
         }
+
+        if (finalState == OVERVIEW) {
+            focusMostRecentTaskForDpadIfNeeded();
+        }
     }
 
     @Override
@@ -184,6 +189,41 @@ public class LauncherRecentsView extends RecentsView<QuickstepLauncher, Launcher
         boolean result = super.onTouchEvent(ev);
         // Do not let touch escape to siblings below this view.
         return result || mActivity.getStateManager().getState().overviewUi;
+    }
+
+    private void focusMostRecentTaskForDpadIfNeeded() {
+        if (isInTouchMode() || isKeyboardTaskFocusPending()) {
+            return;
+        }
+
+        View currentFocus = findFocus();
+        if (currentFocus instanceof TaskView) {
+            return;
+        }
+
+        post(() -> {
+            if (isInTouchMode() || isKeyboardTaskFocusPending()) {
+                return;
+            }
+
+            View focused = findFocus();
+            if (focused instanceof TaskView) {
+                return;
+            }
+
+            TaskView mostRecent = getCurrentPageTaskView();
+            if (mostRecent == null) {
+                mostRecent = getRunningTaskView();
+            }
+            if (mostRecent == null) {
+                mostRecent = getTaskViewNearestToCenterOfScreen();
+            }
+
+            if (mostRecent != null && mostRecent.isAttachedToWindow()) {
+                mostRecent.requestFocus();
+                mostRecent.requestAccessibilityFocus();
+            }
+        });
     }
 
     @Override
